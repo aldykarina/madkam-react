473ef64d0af39c1b00ba442ef2712f14f69a6f0f
Aldy Cordero <aldykarinacp@gmail.com>
2023-04-30 15:38:27 -0300

    PreEntrega1+Cordero

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './scss/app.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as bootstrap from 'bootstrap';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
